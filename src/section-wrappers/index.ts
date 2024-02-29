5e2aa6e4adbd81c3638d5efce59a7c89c574d068
diontugit <diontugit@gmail.com>
2024-02-29 18:43:13 -0500

    minor: modify background

--- src/section-wrappers/index.ts
+++ src/section-wrappers/index.ts
@@ -1,5 +1,9 @@
 import SimpleWrapper from './SimpleWrapper.astro'
+import Wave from './background/Wave.astro'
 
 export default {
     Simple: SimpleWrapper,
+    Background: {
+        Wave: Wave,
+    },
 }
