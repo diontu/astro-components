e26acfaef8b983d27130ab27ddc43416bfffaa4a
diontugit <diontugit@gmail.com>
2024-03-14 21:44:49 -0400

    minor: add sections

--- src/section-wrappers/index.ts
+++ src/section-wrappers/index.ts
@@ -1,9 +1,11 @@
 import SimpleWrapper from './SimpleWrapper.astro'
 import Wave from './background/Wave.astro'
+import Blurred from './background/Blurred.astro'
 
 export default {
     Simple: SimpleWrapper,
     Background: {
         Wave: Wave,
+        Blurred: Blurred,
     },
 }
