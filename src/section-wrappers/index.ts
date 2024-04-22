21424acb7547e41e7eff1a5186590db04acb3ed4
diontugit <diontugit@gmail.com>
2024-04-21 23:13:22 -0400

    minor: minor adjustments

--- src/section-wrappers/index.ts
+++ src/section-wrappers/index.ts
@@ -1,11 +1,13 @@
 import SimpleWrapper from './SimpleWrapper.astro'
 import Wave from './background/Wave.astro'
 import Blurred from './background/Blurred.astro'
+import HeroPic from './background/HeroPic.astro'
 
 export default {
     Simple: SimpleWrapper,
     Background: {
         Wave: Wave,
         Blurred: Blurred,
+        HeroPic: HeroPic,
     },
 }
